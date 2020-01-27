1a82893ea9dab57688d5c608a2c1215519361a97
VAMSI KRISHNA MYALAPALLI <vamsikrishna.vasu@gmail.com>
2020-01-26 20:02:11 -0800

    Create Bon Appétit.java

---
+++ Bon Appétit.java
@@ -0,0 +1,15 @@
+public class Solution {
+
+    // Complete the bonAppetit function below.
+    static void bonAppetit(List<Integer> bill, int k, int charged) {
+                Integer total=bill.stream().collect(Collectors.summingInt(Integer::intValue));
+        int actualShare = total/2;
+        int adjShare = (total-bill.get(k))/2;
+        int diff=actualShare-adjShare;
+        if(adjShare==charged){
+            System.out.println("Bon Appetit");
+        }
+        else{
+            System.out.println(diff);
+        }
+    }
